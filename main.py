d7abe30f0cbde3e0147dcfb806a708e06e199438
Avery R. <avreed.tech@gmail.com>
2025-04-12 09:49:11 -0400

    Added new CONSTANTS to constants.py for FPS and the colors black and white, added a player class that inherits from CircleShape to get circle hitbox's even though we are drawing the player as a triangle to the game screen space. Updated the game loop logic to spawn the player in on the black background as a white triangle sprite entity.

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_FPS
 from constants import ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    #setting up initalization for the game and game window
+    pygame.init()
+    player_sprite = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    
+    # ♾️ While - Our Game Loop
     while True:
-        screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill("black")
+        player_sprite.draw(screen)
         pygame.display.flip()
+
+        # pause game for 1/60th of a second and get the delta return time of .tick() / 1000 to get
+        # seconds from the returning milliseconds value
+        dt = clock.tick(SCREEN_FPS) / 1000
+        print(dt) 
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,22 @@
+import pygame
+from constants import PLAYER_RADIUS, PLAYER_LINE_WIDTH, COLOR_WHITE
+from circleshape import CircleShape
+
+
+# Base class for player objects
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+    
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, COLOR_WHITE, self.triangle(), PLAYER_LINE_WIDTH)
