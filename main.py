e943cbeed1ee355c494b8988639dd2893318cd9d
Avery R. <avreed.tech@gmail.com>
2025-04-18 13:49:46 -0400

    - Added the ability to rotate the player sprite in place - No thanks is needed it only took a few days because i was procrastinating LMAO

--- main.py
+++ main.py
@@ -5,32 +5,33 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_FPS
 from constants import ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+   print("Starting Asteroids!")
+   print(f"Screen width: {SCREEN_WIDTH}")
+   print(f"Screen height: {SCREEN_HEIGHT}")
     
     #setting up initalization for the game and game window
-    pygame.init()
-    player_sprite = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #set player sprite cords(x, y) to middle of screen
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-    dt = 0
+   pygame.init()
+   player_sprite = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #set player sprite cords(x, y) to middle of screen
+   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+   clock = pygame.time.Clock()
+   dt = 0
     
     
-    # ♾️ While - Our Game Loop
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+    # ♾️ While - The Game Loop
+   while True:
+      for event in pygame.event.get():
+         if event.type == pygame.QUIT:
+            return
             
-        screen.fill(COLOR_BLACK)
-        player_sprite.draw(screen)
-        pygame.display.flip() #refreshes the screen | flip to next frame
+      screen.fill(COLOR_BLACK)
+      player_sprite.draw(screen)
+      player_sprite.update(dt)
+      pygame.display.flip() #refreshes the screen | flip to next frame | Renders Screen which is the Game Loop's State
 
-        # pause game for 1/60th of a second and get the delta return time of .tick() / 1000 to get
-        # seconds from the returning milliseconds value
-        dt = clock.tick(SCREEN_FPS) / 1000
-        print(dt) 
+      # pause game for 1/60th of a second and get the delta return time of .tick() / 1000 to get
+      # seconds from the returning milliseconds value
+      dt = clock.tick(SCREEN_FPS) / 1000 # 60/FPS
+      print(dt)
 
 if __name__ == "__main__":
     main()
