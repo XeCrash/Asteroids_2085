69dd6d97476b0deff6c714dfb8b414469dc0f23d
Avery R. <avreed.tech@gmail.com>
2025-04-18 20:56:03 -0400

    Changes: - Added astoidfield and astroid classes - Added spawning astroids that fly in from random edges of the screen - Added a static containers class to Asteroid and AsteroidField classes to allow management through pygame groups
    Files Changed:
    - new file:   asteroid.py
    - new file:   asteroidfield.py
    - modified:   constants.py
    - modified:   main.py
    git commit -m

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import sys
 import pygame
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 from player import Player
 from constants import COLOR_BLACK
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_FPS
-from constants import ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+from constants import ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS, ASTEROID_X_AXIS_MIN_VELOCITY_RIGHT
 
 def main():
    print("Starting Asteroids!")
    print(f"Screen width: {SCREEN_WIDTH}")
    print(f"Screen height: {SCREEN_HEIGHT}")
     
-    #setting up initalization for the game and game window
+   #setting up initalization for the game and game window
    pygame.init()
-   updatable = pygame.sprite.Group()
-   drawable = pygame.sprite.Group()
-   Player.containers = (updatable, drawable)
-   player_sprite = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #set player sprite cords(x, y) to middle of screen
+   updatable_group = pygame.sprite.Group()
+   drawable_group = pygame.sprite.Group()
+   asteroids_group = pygame.sprite.Group()
+   Player.containers = (updatable_group, drawable_group)
+   Asteroid.containers = (asteroids_group, updatable_group, drawable_group)
+   AsteroidField.containers = (updatable_group)
+   Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #set player sprite cords(x, y) to middle of screen
+   astroid_class = Asteroid(-100, 100, ASTEROID_MIN_RADIUS)
+   astroid_class.velocity = pygame.Vector2(ASTEROID_X_AXIS_MIN_VELOCITY_RIGHT, 0) #move 20 units to the right 0 units down
+   AsteroidField()
    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
    clock = pygame.time.Clock()
    dt = 0
    
-   for thing in updatable:
+   for thing in asteroids_group:
       print(f"Thing: {thing}")
    
    # ♾️ While - The Game Loop
@@ -30,9 +38,9 @@ def main():
             return
             
       screen.fill(COLOR_BLACK)
-      for p1_sprite in drawable:
-         p1_sprite.draw(screen)
-      updatable.update(dt) #Updates the rotation of player sprite
+      for sprites in drawable_group:
+         sprites.draw(screen)
+      updatable_group.update(dt) #Updates the rotation of player sprite
       pygame.display.flip() #refreshes the screen | flip to next frame | Renders Screen which is the Game Loop's State
 
       # pause game for 1/60th of a second and get the delta return time of .tick() / 1000 to get
